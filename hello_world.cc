d1a33201a2e11f4809a971bf64178d8f49c29c96
Valeria Bosch <alu0101485287@ull.edu.es>
2021-10-03 23:03:35 +0000

    Primer hello world

---
+++ hello_world.cc
@@ -0,0 +1,7 @@
+// Práctica 1 El Entorno de trabajo IaaS para la asignatura
+// Valeria Bosch Pérez (alu0101485287)
+#include <iostream>
+
+int main () {
+    std::cout << "Hello world :D" << std::endl;
+}
